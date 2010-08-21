3cb2d36c47c420725316e913bd5b0942c3e2e902
craig9 <craig9@54b3f195-e847-2ee0-2615-ceb133cd3927>
2010-08-21 17:57:33 +0000

    Refactored and commented code, refactored interface, added a useful error message if the user chooses "selected only", and doesn't actually select anything

--- replace_font.py
+++ replace_font.py
@@ -21,8 +21,11 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 -----------------------
 
 This script finds all fonts in the current drawing that match the 
-specified find font, and replaces it with the specified replacement
+specified find font, and replaces them with the specified replacement
 font.
+
+It can also replace all fonts indiscriminately, and list all fonts
+currently being used.
 '''
 
 import os
@@ -30,12 +33,110 @@ import sys
 import inkex
 import simplestyle
 
-tags_to_check = ['{http://www.w3.org/2000/svg}tspan',
+text_tags = ['{http://www.w3.org/2000/svg}tspan',
 							'{http://www.w3.org/2000/svg}text',
 							'{http://www.w3.org/2000/svg}flowRoot',
 							'{http://www.w3.org/2000/svg}flowPara',
 							'{http://www.w3.org/2000/svg}flowSpan']
-attributes_to_check = ['font-family', '-inkscape-font-specification']
+font_attributes = ['font-family', '-inkscape-font-specification']
+
+def set_font(node, new_font, style=None):
+	'''
+	Sets the font attribute in the style attribute of node, using the 
+	font name stored in new_font. If the style dict is open already,
+	it can be passed in, otherwise it will be optned anyway.
+
+	Returns a dirty boolean flag
+	'''
+	dirty = False
+	if not style:
+		style = get_style(node)
+	for att in font_attributes:
+		if att in style:
+			style[att] = new_font
+			set_style(node, style)
+			dirty = True
+	return dirty
+
+def find_replace_font(node, find, replace):
+	'''
+	Searches the relevant font attributes/styles of node for find, and
+	replaces them with replace.
+
+	Returns a dirty boolean flag
+	'''
+	dirty = False
+	style = get_style(node)
+	for att in font_attributes:
+		if att in style and style[att].strip().lower() == find:
+			set_font(node, replace, style)
+			dirty = True
+	return dirty
+
+def is_styled_text(node):
+	'''
+	Returns true if the tag in question is a "styled" element that
+	can hold text.
+	'''
+	return node.tag in text_tags and 'style' in node.attrib
+
+def is_text(node):
+	'''
+	Returns true if the tag in question is an element that
+	can hold text.
+	'''
+	return node.tag in text_tags
+
+
+def get_style(node):
+	'''
+	Sugar coated way to get style dict from a node
+	'''
+	return simplestyle.parseStyle(node.attrib['style'])
+
+def set_style(node, style):
+	'''
+	Sugar coated way to set the style dict, for node
+	'''
+	node.attrib['style'] = simplestyle.formatStyle(style)
+
+def get_fonts(node):
+	'''
+	Given a node, returns a list containing all the fonts that
+	the node is using.
+	'''
+	fonts = []
+	s = get_style(node)
+	for a in font_attributes:
+		if a in s:
+			fonts.append(s[a])
+	return fonts
+
+def die(msg = "Dying!"):
+	inkex.errormsg(msg)
+	sys.exit(0)
+
+def report_replacements(num):
+	'''
+	Sends a message to the end user showing success of failure
+	of the font replacement
+	'''
+	if num == 0:
+		die('Couldn\'t find anything using that font, please ensure the spelling and spacing is correct.')
+	else:
+		inkex.errormsg("Those fonts were successfully replaced")
+
+def report_findings(findings):
+	'''
+	Tells the user which fonts were found, if any
+	'''
+	if len(findings) == 0:
+		inkex.errormsg("Didn't find any fonts in this document/selection.")
+	else:
+		if len(findings) == 1:
+			inkex.errormsg("Found the following font only: %s" % findings[0])
+		else:
+			inkex.errormsg("Found the following fonts:\n%s" % '\n'.join(findings))
 
 class ReplaceFont(inkex.Effect):
 	'''
@@ -43,96 +144,104 @@ class ReplaceFont(inkex.Effect):
 	'''
 	def __init__(self):
 		inkex.Effect.__init__(self)	
-		self.OptionParser.add_option("--find", action="store", 
-										type="string", dest="find",
+		self.OptionParser.add_option("--fr_find", action="store", 
+										type="string", dest="fr_find",
 										default=None, help="")
 										
-		self.OptionParser.add_option("--replace", action="store", 
-										type="string", dest="replace", 
-										default=None, help="")
-		
-		self.OptionParser.add_option("--selected_only", action="store", 
-										type="inkbool", dest="selected_only", 
+		self.OptionParser.add_option("--fr_replace", action="store", 
+										type="string", dest="fr_replace", 
 										default=None, help="")
 
-		self.OptionParser.add_option("--every", action="store", 
-										type="inkbool", dest="every", 
+		self.OptionParser.add_option("--r_replace", action="store", 
+										type="string", dest="r_replace", 
 										default=None, help="")
 
-		self.OptionParser.add_option("--list_all", action="store", 
-										type="inkbool", dest="list_all", 
+		self.OptionParser.add_option("--action", action="store", 
+										type="string", dest="action", 
 										default=None, help="")
-		self.selected_items = []
 
-	def report_replacements(self, num):
-		if num == 0:
-			inkex.errormsg('Couldn\'t find anything using that font, please ensure the spelling and spacing is correct.')
-			sys.exit(0)
-		else:
-			if num > 1:
-				occurrences = "occurrences"
-			else:
-				occurrences = "occurrence"
-			inkex.errormsg("Successfully replaced %d %s of that font" % (num, occurrences))
-
-	def report_findings(self, findings):
-		if len(findings) == 0:
-			inkex.errormsg('Couldn\'t find a single font in this document/selection.')
-		else:
-			if len(findings) == 1:
-				inkex.errormsg("Found the following font only: %s" % findings[0])
-			else:
-				inkex.errormsg("Found the following fonts:\n%s" % '\n'.join(findings))
+		self.OptionParser.add_option("--scope", action="store", 
+										type="string", dest="scope", 
+										default=None, help="")
 
 	def find_child_text_items(self, node):
-		if node.tag in tags_to_check:
+		'''
+		Recursive method for appending all text-type elements
+		to self.selected_items
+		'''
+		if is_text(node):
 			self.selected_items.append(node)
 			for child in node:
 				self.find_child_text_items(child)
 
-	def relevant_items(self):
-		if self.options.selected_only:
+	def relevant_items(self, scope):
+		'''
+		Depending on the scope, returns all text elements, or all 
+		selected text elements including nested children
+		'''
+		items = []
+		to_return = []
+		if scope == "selection_only":
 			self.selected_items = []
 			for item in self.selected.iteritems():
 				self.find_child_text_items(item[1])
-			return self.selected_items
+			items = self.selected_items
+			if len(items) == 0:
+				die("There was nothing selected")
 		else:
-			return self.document.getroot().getiterator()
+			items = self.document.getroot().getiterator()
+		to_return.extend(filter(is_text, items))
+		return to_return
 
-	def find_replace(self):
-		replacements_made = 0
-		for el in self.relevant_items():
-			if el.tag in tags_to_check and 'style' in el.attrib:		
-				replaced = False
-				style_dict = simplestyle.parseStyle(el.attrib['style'])
-				for att in attributes_to_check:
-					if att in style_dict and (style_dict[att].strip().lower() == self.options.find or self.options.every):
-						style_dict[att] = self.options.replace
-						el.attrib['style'] = simplestyle.formatStyle(style_dict)
-						replaced = True
-				if replaced:
-					replacements_made += 1
-		self.report_replacements(replacements_made)
+	def find_replace(self, nodes, find, replace):
+		'''
+		Walks through nodes, replacing fonts as it goes according
+		to find and replace
+		'''
+		replacements = 0
+		for node in nodes:
+			if find_replace_font(node, find, replace):
+				replacements += 1
+		report_replacements(replacements)
 
-	def list_all(self):
+	def replace_all(self, nodes, replace):
+		'''
+		Walks through nodes, setting fonts indiscriminately.
+		'''
+		replacements = 0
+		for node in nodes:
+			if set_font(node, replace):
+				replacements += 1
+		report_replacements(replacements)
+
+	def list_all(self, nodes):
+		'''
+		Walks through nodes, building a list of all fonts found, then 
+		reports to the user with that list
+		'''
 		fonts_found = []
-		for el in self.relevant_items():
-			if el.tag in tags_to_check and 'style' in el.attrib:
-				style_dict = simplestyle.parseStyle(el.attrib['style'])
-				for att in attributes_to_check:
-					if att in style_dict:
-						new_font = style_dict[att]
-						if not new_font in fonts_found:
-							fonts_found.append(new_font)
-		self.report_findings(fonts_found)
+		for node in nodes:
+			for f in get_fonts(node):
+				if not f in fonts_found:
+					fonts_found.append(f)
+		report_findings(sorted(fonts_found))
 
 	def effect(self):
-		if self.options.list_all:
-			self.list_all()
+		action = self.options.action.strip("\"") # TODO Report this bug (Extra " characters)
+		scope = self.options.scope
+
+		relevant_items = self.relevant_items(scope)
+
+		if action == "find_replace":
+			find = self.options.fr_find.strip().lower()
+			replace = self.options.fr_replace
+			self.find_replace(relevant_items, find, replace)
+		elif action == "replace_all":
+			replace = self.options.r_replace
+			self.replace_all(relevant_items, replace)
+		elif action == "list_only":
+			self.list_all(relevant_items)
 			sys.exit(0)
-		else:
-			self.options.find = self.options.find.strip().lower()
-			self.find_replace()
 
 if __name__ == "__main__":
 	e = ReplaceFont()
